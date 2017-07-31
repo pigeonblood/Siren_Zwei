4f078b6c74c785b16035bf3d74290270eeb3ef9e
frodo821 <1234567890.sakai.jp@gmail.com>
2017-07-31 15:01:20 +0900

    Update main.py

--- Python/driver/main.py
+++ Python/driver/main.py
@@ -1,4 +1,10 @@
 #-*-coding:utf-8;-*-
-
+#importing modules
 import sys
 sys.path.append("../framework")
+import moduleLoader as ml
+
+#internal modules into dictionary
+#対象ディレクトリのモジュールをすべて自動的に読み込み
+#モジュールへのアクセス方法: mod[<モジュール名>]
+mod = ml.LoadModules()
